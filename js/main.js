9ac16867803c7838c60d700eb1ca2906689279b2
Lawyer-web <web2020@ukr.net>
2020-10-21 18:54:48 +0300

    Доработка первого задания личного проекта

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var offer = {
+const offer = {
    author: {
         avatar:`img/avatars/user01.png`,
     },
@@ -20,84 +20,114 @@ var offer = {
     }
 };
 
-var TITLES = ['Квартира 1', 'Квартира 2', 'Квартира 3', 'Квартира 4', 'Квартира 5', 'Квартира 6', 'Квартира 7', 'Квартира 8'];
-var TYPES = ['palace', 'flat', 'house', 'bungalow'];
-var TIMES = ['12:00', '13:00', '14:00'];
-var OPTIONS = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var PHOTO = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const TIMES = ['12:00', '13:00', '14:00'];
+const OPTIONS = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const PHOTO = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
-var map = document.querySelector('.map');
-var pinMap = document.querySelector('.map__pins');
-var template = document.querySelector('#pin').content.querySelector('.map__pin');
+const PINWIDTH = 50;
+const PINHEIGT = 70;
+
+const map = document.querySelector('.map');
+const pinMap = document.querySelector('.map__pins');
+const pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 /* 1.Функция генерации случайного числа */
 
-var getRandomInt = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomInt = (min, max)=>Math.floor(Math.random() * (max - min + 1)) + min;
 
 /* 2.Функция выбора случайного элемента массива */
 
-var getRandomArrElem = function (arr) {
-  var rand = getRandomInt(0, arr.length - 1);
-  return arr[rand];
-};
+const getRandomElement =(items)=>items[getRandomInt(0, items.length - 1)];
 
 /* 3. Функция перетасовки массива */
 
-var getMixArr = function (arr) {
-  var mixArr = arr.slice().sort(function () {
-    return 0.5 - Math.random();
+const getMixArr =(items)=>items.slice().sort( ()=> {0.5 - Math.random();
   });
-  return mixArr;
-};
 
 /* 4.Функция генерации объекта с объявлением */
 
-var generatePost = function (number) {
-  var post = {
-    'author': {
-      'avatar': 'img/avatars/user0' + (number + 1) + '.png'
+const Price = {
+  MIN: 1000,
+  MAX:100000
+};
+
+const Random = {
+  MIN: 130,
+  MAX:630
+};
+
+const Rooms={
+  MIN:1,
+  MAX:5
+};
+
+const Quests = {
+  MIN: 1,
+  MAX:10
+};
+
+const generatePost = (number) => {
+  const location = {
+    x: getRandomInt(0, map.offsetWidth),
+    y: getRandomInt(Random.MIN,Random.MAX )
+  };
+  const post = {
+    author: {
+      avatar: `img/avatars/user0${number}.png`
     },
-    'offer': {
-      'title': TITLES[number],
-      'address': '600, 350',
-      'price': getRandomInt(1000, 100000),
-      'type': getRandomArrElem(TYPES),
-      'rooms': getRandomInt(1, 5),
-      'guests': getRandomInt(1, 10),
-      'checkin': getRandomArrElem(TIMES),
-      'checkout': getRandomArrElem(TIMES),
-      'feautures': getMixArr(OPTIONS).slice(0, getRandomInt(1, OPTIONS.length)),
-      'description': 'строка с описанием',
-      'photos': getMixArr(PHOTO).slice(0, getRandomInt(1, PHOTO.length))
+    offer: {
+      title: `Квартира ${number}`,
+      address: `${location.x}, ${location.y }`,
+      price: getRandomInt(Price.MIN, Price.MAX),
+      type: getRandomElement(TYPES),
+      rooms: getRandomInt(Rooms.MIN,Rooms.MAX),
+      guests: getRandomInt(Quests.MIN, Quests.MAX),
+      checkin: getRandomElement(TIMES),
+      checkout: getRandomElement(TIMES),
+      features: getMixArr(OPTIONS).slice(0, getRandomInt(1, OPTIONS.length)),
+      description: `строка с описанием`,
+      photos: getMixArr(PHOTO).slice(0, getRandomInt(1, PHOTO.length))
     },
-    'location': {
-      'x': getRandomInt(0, map.offsetWidth),
-      'y': getRandomInt(130, 630)
-    }
+    location
   };
   return post;
 };
 
 /* 5.Функция создания массива случайных объявлений */
-var generateData = function () {
-  var data = [];
-  for (var i = 0; i < 8; i++) {
+const generatePosts =()=> {
+  const data = [];
+  for (var i = 1; i <= 8; i++) {
     console.log(i);
     data.push(generatePost(i));
   }
   return data;
 };
-var data = generateData();
-console.log(data);
+const posts = generatePosts();
+console.log(posts);
 
 /* 6. Функция переключения карты в активное состояние */
-var activeMap = function () {
+const activeMap =()=> {
   map.classList.remove('map--faded');
 };
+activeMap();
 
 /* 7. Функция сoздания метки случайного объявления */
+
+const pinsFragment = document.createDocumentFragment();
+for (let post of posts) {
+  const clonedPinTemplate = pinTemplate.cloneNode(true);
+  clonedPinTemplate.style = `left:${post.location.x-PINWIDTH/2}px; top: ${post.location.y-PINHEIGT}px;`
+  clonedPinTemplate.querySelector('img').setAttribute('src', post.author.avatar);
+  clonedPinTemplate.querySelector('img').setAttribute('alt', post.offer.title);
+  pinsFragment.appendChild(clonedPinTemplate);
+  console.log(post,clonedPinTemplate);
+}
+pinMap.appendChild(pinsFragment);
+
+
+
+
 
 
 // const data = [];
