5c424956e967b4bedfb2d0c37a0b9cad5c4c2886
Lawyer-web <web2020@ukr.net>
2020-10-22 15:32:01 +0300

    Исправил все ошибки первого задания личного проекта

---
+++ js/card.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Параметры фотографий жилья
+
+const Photo = {
+  WIDTH: 45,
+  HEIGHT: 40
+};
+
+// Типы жилья
+
+const typesHousing = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало'
+};
--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
 'use strict';
 
-const offer = {
-   author: {
-        avatar:`img/avatars/user01.png`,
-    },
-    offer: {
-        "title": `Квартира 1`,
-        "address":`600, 350`,
-        "price": 1000,
-        "type": `palace`,
-        "rooms": 5,
-        "guests": 10,
-        "checkin": `14:00`,
-        "checkout": `13:00,`
-    },
-    "location": {
-        "x": 10,
-        "y": 130
-    }
-};
-
 const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+
 const TIMES = ['12:00', '13:00', '14:00'];
+
 const OPTIONS = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+
 const PHOTO = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
-const PINWIDTH = 50;
-const PINHEIGT = 70;
+const Pin = {
+  WIDTH: 50,
+  HEIGHT: 70
+};
 
 const map = document.querySelector('.map');
 const pinMap = document.querySelector('.map__pins');
@@ -38,39 +23,42 @@ const getRandomInt = (min, max)=>Math.floor(Math.random() * (max - min + 1)) + m
 
 /* 2.Функция выбора случайного элемента массива */
 
-const getRandomElement =(items)=>items[getRandomInt(0, items.length - 1)];
+const getRandomElement = (items)=>items[getRandomInt(0, items.length - 1)];
 
 /* 3. Функция перетасовки массива */
 
-const getMixArr =(items)=>items.slice().sort( ()=> {0.5 - Math.random();
+const getMixArr = (items)=> {
+  return items.slice().sort(function () {
+    return Math.random() - 0.5;
   });
+};
 
 /* 4.Функция генерации объекта с объявлением */
 
 const Price = {
   MIN: 1000,
-  MAX:100000
+  MAX: 100000
 };
 
 const Random = {
   MIN: 130,
-  MAX:630
+  MAX: 630
 };
 
-const Rooms={
-  MIN:1,
-  MAX:5
+const Rooms = {
+  MIN: 1,
+  MAX: 5
 };
 
 const Quests = {
   MIN: 1,
-  MAX:10
+  MAX: 10
 };
 
 const generatePost = (number) => {
   const location = {
     x: getRandomInt(0, map.offsetWidth),
-    y: getRandomInt(Random.MIN,Random.MAX )
+    y: getRandomInt(Random.MIN, Random.MAX)
   };
   const post = {
     author: {
@@ -81,7 +69,7 @@ const generatePost = (number) => {
       address: `${location.x}, ${location.y }`,
       price: getRandomInt(Price.MIN, Price.MAX),
       type: getRandomElement(TYPES),
-      rooms: getRandomInt(Rooms.MIN,Rooms.MAX),
+      rooms: getRandomInt(Rooms.MIN, Rooms.MAX),
       guests: getRandomInt(Quests.MIN, Quests.MAX),
       checkin: getRandomElement(TIMES),
       checkout: getRandomElement(TIMES),
@@ -95,19 +83,17 @@ const generatePost = (number) => {
 };
 
 /* 5.Функция создания массива случайных объявлений */
-const generatePosts =()=> {
+const generatePosts = ()=> {
   const data = [];
-  for (var i = 1; i <= 8; i++) {
-    console.log(i);
+  for (let i = 1; i <= 8; i++) {
     data.push(generatePost(i));
   }
   return data;
 };
 const posts = generatePosts();
-console.log(posts);
 
 /* 6. Функция переключения карты в активное состояние */
-const activeMap =()=> {
+const activeMap = ()=> {
   map.classList.remove('map--faded');
 };
 activeMap();
@@ -117,45 +103,9 @@ activeMap();
 const pinsFragment = document.createDocumentFragment();
 for (let post of posts) {
   const clonedPinTemplate = pinTemplate.cloneNode(true);
-  clonedPinTemplate.style = `left:${post.location.x-PINWIDTH/2}px; top: ${post.location.y-PINHEIGT}px;`
+  clonedPinTemplate.style = `left:${post.location.x - Pin.WIDTH / 2}px; top: ${post.location.y - Pin.HEIGT}px`;
   clonedPinTemplate.querySelector('img').setAttribute('src', post.author.avatar);
   clonedPinTemplate.querySelector('img').setAttribute('alt', post.offer.title);
   pinsFragment.appendChild(clonedPinTemplate);
-  console.log(post,clonedPinTemplate);
 }
 pinMap.appendChild(pinsFragment);
-
-
-
-
-
-
-// const data = [];
-
-// for (var i = 0; i < 8; i++) {
-//   console.log(i);
-//   data.push({
-//    author: {
-//         avatar:`img/avatars/user0${i}.png`,
-//     },
-//     offer: {
-//         "title": (TITLES[i]),
-//         "address":`600, 350`,
-//         "price": getRandomInt(1000,100000),
-//         "type":getRandomArrElem(TYPES) ,
-//         "rooms": getRandomInt(1,5),
-//         "guests": getRandomInt(1,10),
-//         "checkin": getRandomArrElem(TIMES),
-//         "checkout": getRandomArrElem(TIMES),
-//         "feautures": getMixArr(OPTIONS).slice(0, getRandomInt(1, OPTIONS.length)),
-//         "description": `строка с описанием`,
-//         "photos": getMixArr(PHOTO).slice(0, getRandomInt(1, PHOTO.length))
-//     },
-//         "location": {
-//         "x": 10,
-//         "y":getRandomInt(130,630)
-//       }
-
-// });
-// }
-// console.log(data);
